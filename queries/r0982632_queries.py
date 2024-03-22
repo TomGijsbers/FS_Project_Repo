f5516373f228ec086cc6d84d0bef45a145bb26ca
Geert Vuurstaek <r0982632@student.thomasmore.be>
2024-03-22 18:28:37 +0100

    toevoegen endpoints, queries en models

---
+++ queries/r0982632_queries.py
@@ -0,0 +1,6 @@
+#Geert Vuurstaek
+
+insert_applications_query = 'INSERT INTO scoopheaven.applicationform (firstName, lastName, birthdate, gender, emailaddress) VALUES (%s, %s, %s, %s, %s);'
+
+scoopheaven_reviews = 'SELECT c.name, c.avatar, r.reviewText, r.rating, r.time FROM scoopheaven.reviews r JOIN customers c ON r.customersId = c.customersId;'
+
