a767b9073163d5fde417811e874117afeee33c97
TomGijsbers <r0878924@student.thomasmore.be>
2024-03-30 12:20:12 +0100

    get photos

--- routes/get_r0878924_endpoints.py
+++ routes/get_r0878924_endpoints.py
@@ -1,3 +1,23 @@
 from fastapi import APIRouter
+import database
+import queries.r0878924_queries
 
 app = APIRouter()
+
+
+@app.get("/photos")
+def get_photos():
+    query = queries.r0878924_queries.photo_query
+    result = database.execute_sql_query(query)
+    if isinstance(result, Exception):
+        return result, 500
+    photo_list = []
+    for photo_details in result:
+        photo_list.append({
+            "id": photo_details[0],
+            "name": photo_details[1],
+            "path": photo_details[2],
+            "description": photo_details[3],
+            "upload_date": photo_details[4]
+        })
+    return {"photos": photo_list}
