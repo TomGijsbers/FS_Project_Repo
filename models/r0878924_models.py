cd4c2a5f96b6b4a14f2bc1b290be9f9b9bb3444a
TomGijsbers <r0878924@student.thomasmore.be>
2024-03-30 15:53:02 +0100

    contactform post

--- models/r0878924_models.py
+++ models/r0878924_models.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+# class uploadPhoto(BaseModel):
+#     id: int
+#     name: str
+#     path: str
+#     description: str
+#     upload_date : date
 
+
+class contact(BaseModel):
+    contact_id: int
+    first_name_sender: str
+    last_name_sender: str
+    email_sender: str
+    message: str
+    created_at: date
