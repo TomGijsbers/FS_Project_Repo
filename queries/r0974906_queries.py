0db08e71eb3c33a126e415943df10173eacade18
michaelmccraith <r0974906@student.thomasmore.be>
2024-04-07 13:39:10 +0200

    added query

--- queries/r0974906_queries.py
+++ queries/r0974906_queries.py
@@ -1,3 +1,4 @@
 flavours_query = "SELECT FlavorID, FlavorName, Price, Availability, Vegan, SugarFree FROM scoopheaven.flavors;"
 holders_query = "SELECT HolderID, Type, Price, Availability, Vegan, SugarFree FROM scoopheaven.holders;"
 toppings_query = "SELECT ToppingID, Type, Price, Availability, Vegan, SugarFree FROM scoopheaven.toppings;"
+quotes_query = "SELECT quote FROM scoopheaven.icecreamquotes;"
