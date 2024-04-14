8f5cf149d1491788325960e400fbdb758aed77e2
michaelmccraith <r0974906@student.thomasmore.be>
2024-04-14 10:58:14 +0200

    further changes

--- routes/post_r0974906_endpoints.py
+++ routes/post_r0974906_endpoints.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 
 app = APIRouter()
 
-@app.post("/answer")
-def create_answer(answer: r0974906_models.Answer):
-    answer_query = queries.r0974906_queries.insert_answer_query
-    success_answer_query = database.execute_sql_query(answer_query, (answer.email, answer.flavour, answer.reason,))
+@app.post("/reasons")
+def create_answer(reasons: r0974906_models.Answer):
+    reasons_query = queries.r0974906_queries.insert_reasons_query
+    success_answer_query = database.execute_sql_query(reasons_query, (reasons.email, reasons.flavour, reasons.reason,))
     if success_answer_query:
         return answer
